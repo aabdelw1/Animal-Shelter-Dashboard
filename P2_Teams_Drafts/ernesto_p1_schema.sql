58b8c7a24942a94305b7d7ed70ed7c845d7e6ad5
Lopez Chan, Ernesto Rene <echan39@gatech.edu>
2020-02-27 19:02:44 -0600

    Create ernesto_p1_schema.sql

---
+++ P2_Teams_Drafts/ernesto_p1_schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Shelter.Adopter (
+  Email_Address VARCHAR(250) NOT NULL,
+  Phone_Number INT NOT NULL,
+  Street VARCHAR(50) NOT NULL,
+  City VARCHAR(50) NOT NULL,
+  State VARCHAR(50) NOT NULL,
+  ZIPCode VARCHAR(50) NOT NULL
+  Applicant_Fist_Name VARCHAR(100) NOT NULL
+  Applicant_Last_Name VARCHAR(100) NOT NULL
+  PRIMARY KEY (Email_Address),
+);
+
+CREATE TABLE Shelter.AdoptionApplication (
+  Application_Number INT NOT NULL,
+  Email_Address VARCHAR(50) NOT NULL,
+  Date_Of_Application DATE NOT NULL,
+  CoApplicant_First_Name VARCHAR(100) NOT NULL,
+  CoApplicant_Last_Name VARCHAR(100) NOT NULL,
+  State ENUM('Pending Approval', 'Approved', 'Rejected') DEFAULT 'Pending Approval',
+  PRIMARY KEY (Application_Number),
+  KEY(Email_Address),
+);
+
+-- Constraints   Foreign Keys: FK_ChildTable_childColumn_ParentTable_parentColumn
+
+ALTER TABLE AdoptionApplication
+  ADD CONSTRAINT fk_AdoptionApplication_EmailAddress_Adopter_EmailAddress FOREIGN KEY (Email_Address) REFERENCES Adopter (Email_Address);
+ 
+
+
+
