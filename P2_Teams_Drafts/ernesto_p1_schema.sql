f79bbc012d6efdaa4df8ea3f2bbdc731ec42a914
Lopez Chan, Ernesto Rene <echan39@gatech.edu>
2020-02-27 23:28:14 -0600

    Update ernesto_p1_schema.sql

--- P2_Teams_Drafts/ernesto_p1_schema.sql
+++ P2_Teams_Drafts/ernesto_p1_schema.sql
@@ -1,22 +1,22 @@
-CREATE TABLE Shelter.Adopter (
+CREATE TABLE Adopter (
   Email_Address VARCHAR(250) NOT NULL,
   Phone_Number INT NOT NULL,
   Street VARCHAR(50) NOT NULL,
   City VARCHAR(50) NOT NULL,
-  State VARCHAR(50) NOT NULL,
+  `State` VARCHAR(50) NOT NULL,
   ZIPCode VARCHAR(50) NOT NULL
   Applicant_Fist_Name VARCHAR(100) NOT NULL
   Applicant_Last_Name VARCHAR(100) NOT NULL
   PRIMARY KEY (Email_Address),
 );
 
-CREATE TABLE Shelter.AdoptionApplication (
+CREATE TABLE AdoptionApplication (
   Application_Number INT NOT NULL,
-  Email_Address VARCHAR(50) NOT NULL,
+  Email_Address VARCHAR(250) NOT NULL,
   Date_Of_Application DATE NOT NULL,
   CoApplicant_First_Name VARCHAR(100) NOT NULL,
   CoApplicant_Last_Name VARCHAR(100) NOT NULL,
-  State ENUM('Pending Approval', 'Approved', 'Rejected') DEFAULT 'Pending Approval',
+  `State` ENUM('Pending Approval', 'Approved', 'Rejected') DEFAULT 'Pending Approval',
   PRIMARY KEY (Application_Number),
   KEY(Email_Address),
 );
