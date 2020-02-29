10c9d5a9759a3135f1f7cffb18882dbc2c1afb1f
Abdelwahed, Ammar Sherif <aabdelwahed3@gatech.edu>
2020-02-29 15:00:03 -0600

    Add files via upload

---
+++ P2_Teams_Drafts/aabdelwahed3.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Shelter.VaccineAdministration (
+    Expiration_Date DATE NOT NULL,
+    Vacination_Number INT NOT NULL,
+    Date_Administired DATE NOT NULL,
+    Vaccine VARCHAR(50) NOT NULL,
+    PRIMARY KEY (Vaccination_Number, Date_Administired)
+
+);
+
+CREATE TABLE Shelter.Vaccines (
+    Vaccine_Type VARCHAR(250) NOT NULL,
+    Require_for_Adoption BOOLEAN NOT NULL
+    PRIMARY KEY (Vaccine_Type)
+);
+
+
+ALTER TABLE Vaccines
+    ADD CONSTRAINT fk_Vaccine
+    FOREIGN KEY (Vaccine_Type) REFERENCES Vaccine(Vaccine_Type)
+
+ALTER TABLE VaccineAdministration
+    ADD CONSTRAINT fk_VaccineAdministration_Vaccine_Type_Vaccines_Vaccine_Type
+    FOREIGN KEY (Vaccine_Type) REFERENCES Vaccine(Vaccine_Type)
+
