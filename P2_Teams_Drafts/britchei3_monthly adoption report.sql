125eeb63971de2215b09dcd6b91c230487e195b9
Brian Ritchie <brianritchie@gatech.edu>
2020-03-01 14:45:27 -0500

    Include empty months in query; add subtotals

--- P2_Teams_Drafts/britchei3_monthly adoption report.sql
+++ P2_Teams_Drafts/britchei3_monthly adoption report.sql
@@ -7,14 +7,26 @@ USE cs6400_sp20_team054;
 -- row-wise (with a column for the month name, species, breed, number of surrenders, and number of adoptions), 
 -- The table should be sorted by month in ascending order (earliest to latest), and species name alphabetically (A-Z) followed by breed name alphabetically (A-Z, 
 --    in the case of an animal with multiple breeds, remember that breeds should be combined into a single value in alphabetical order).
-
+select distinct Yr_Month, Species, Breed_Name, Adoption_Count, Surrender_Count
+FROM (
 select 
-	Yr_Month, 
+	dates.Yr_Month, 
 	Species, 
     Breed_Name,
-	sum(Adoption_Count) as Adoption_Count, 
-    Sum(Surrender_Count) as Surrender_Count
+	IFNULL(SUM(Adoption_Count),0) as Adoption_Count, 
+    IFNULL(Sum(Surrender_Count),0) as Surrender_Count
 FROM
+(
+	with RECURSIVE dates as (
+		select date_add(CURDATE(), interval -12 month) as dt -- start
+		union all
+		select date_add(dt, interval 1 month) as dt
+		from dates
+		where dt < CURDATE() -- end
+	) select EXTRACT(YEAR_MONTH FROM dt) as yr_month
+	from dates
+	where dt between date_add(CURDATE(), interval -12 month) and date_add(CURDATE(), interval -1 month) 
+) dates LEFT JOIN
 (
 	select Yr_Month, Species,
 			Breed_Name,Adoption_Count, Surrender_Count 
@@ -64,6 +76,7 @@ FROM
 				Species,
 				Breed_Name
 		) 
-	) t2
-	GROUP BY Yr_Month, Species, Breed_Name
-ORDER BY Yr_Month, Species, Breed_Name;
+	) t2 on dates.yr_month=t2.yr_month
+	GROUP BY Yr_Month, Species, Breed_Name WITH ROLLUP
+ORDER BY Grouping(dates.Yr_Month), dates.Yr_Month, Grouping(Species), Species, Grouping(Breed_Name), Breed_Name
+) report;
