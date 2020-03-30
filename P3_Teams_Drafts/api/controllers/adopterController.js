835d7e6744b5e0747725929fb35aa449b35f1cfc
Lopez Chan, Ernesto Rene <echan39@gatech.edu>
2020-03-30 00:13:45 -0600

    Update adopterController.js
    All working

--- P3_Teams_Drafts/api/controllers/adopterController.js
+++ P3_Teams_Drafts/api/controllers/adopterController.js
@@ -1,16 +1,16 @@
 exports.get_approved_applications = function(req, res) {
 
     var params = [];
-    var q = "SELECT Application_Number, Applicant_First_Name, Applicant_Last_Name, Street, "+
-    "City, Adopter.`State`, ZIPCode, Phone_Number, Adopter.Email_Address, CoApplicant_First_Name, CoApplicant_Last_Name" + 
-    "FROM Adopter INNER JOIN AdoptionApplication ON Adopter.Email_Address = AdoptionApplication.Email_Address" +
-    "WHERE AdoptionApplication.`State`= 'Approved' AND (Applicant_Last_Name LIKE  ?%) AND (CoApplicant_Last_Name LIKE ?%) AND Application_Number NOT IN" +
-              "(SELECT Adoption_Application_Number"+
-              "FROM Animal"+
-              "WHERE Adoption_Application_Number IS NOT NULL)";
+    var q = `SELECT Application_Number, Applicant_Fist_Name, Applicant_Last_Name, Street,
+    City, Adopter.State, ZIPCode, Phone_Number, Adopter.Email_Address, CoApplicant_First_Name, CoApplicant_Last_Name
+    FROM Adopter INNER JOIN AdoptionApplication ON Adopter.Email_Address = AdoptionApplication.Email_Address
+    WHERE AdoptionApplication.State= 'Approved' AND (Applicant_Last_Name LIKE  ?) AND (CoApplicant_Last_Name LIKE ?) AND Application_Number NOT IN
+              (SELECT Adoption_Application_Number
+              FROM Animal
+              WHERE Adoption_Application_Number IS NOT NULL)`
     
-    params.push(req.params.ApplicantLastName);
-    params.push(req.params.CoApplicantLastName);
+    params.push('%' + req.query.applicantLastName + '%');
+    params.push('%' + req.query.coApplicantLastName + '%');
  
     db.query(q, params, (err, results) => {
         var approvedApplications=[];
@@ -19,7 +19,7 @@ exports.get_approved_applications = function(req, res) {
             results.forEach(a => {
                 approvedApplications.push({
                     applicationNumber: a.Application_Number,
-                    applicantFirstName: a.Applicant_First_Name,
+                    applicantFirstName: a.Applicant_Fist_Name,
                     applicantLastName: a.Applicant_Last_Name,
                     street: a.Street,
                     city: a.City,
@@ -37,65 +37,6 @@ exports.get_approved_applications = function(req, res) {
     });
 };
 
-exports.get_applications_pending_approval = function(req, res) {
-
-    var params = [];
-    var q = `SELECT Application_Number, Applicant_First_Name, Applicant_Last_Name, Street, City, Adopter.State, ZIPCode, Phone_Number, Adopter.Email_Address, CoApplicant_First_Name, CoApplicant_Last_Name 
-    FROM Adopter INNER JOIN AdoptionApplication ON Adopter.Email_Address = AdoptionApplication.Email_Address
-    WHERE AdoptionApplication.State= 'Pending Approval'`
-    
-    db.query(q, params, (err, results) => {
-        var pendingApplications=[];
-
-        if(results!=null) {
-            results.forEach(p => {
-                pendingApplications.push({
-                    applicationNumber: p.Application_Number,
-                    applicantFirstName: p.Applicant_First_Name,
-                    applicantLastName: p.Applicant_Last_Name,
-                    street: p.Street,
-                    city: p.City,
-                    state: p.State,
-                    zipCode: p.ZIPCode,
-                    phoneNumber: p.Phone_Number,
-                    emailAddress: p.Email_Address,
-                    coApplicantFirstName: p.CoApplicant_First_Name,
-                    coApplicantLastName: p.CoApplicant_Last_Name
-                });
-            });
-        }
-        return res.json(pendingApplications);
-    });
-};
-
-
-exports.get_applications_number = function(req, res) {
-
-    var params = [];
-    var q = "SELECT Application_Number"+
-    "FROM AdoptionApplication"+
-    "WHERE Email_Address= ? AND Date_Of_Application = ?"+
-    "AND CoApplicant_First_Name = ? AND CoApplicant_Last_Name = ?";
-    
-    params.push(req.params.EmailAddress);
-    params.push(req.params.DateOfApplication);
-    params.push(req.params.CoApplicantFirstName);
-    params.push(req.params.CoApplicantLastName);
-
-    db.query(q, params, (err, results) => {
-        var applicationNumber=[];
-
-        if(results!=null) {
-            results.forEach(a => {
-                applicationNumber.push({
-                    applicationNumber: a.Application_Number
-                });
-            });
-        }
-        return res.json(applicationNumber);
-    });
-};
-
 exports.post_new_adoption_application = function(req, res) {
     var params = [];
     var q = `INSERT INTO AdoptionApplication (Email_Address, Date_Of_Application, CoApplicant_First_Name, CoApplicant_Last_Name, State)
@@ -109,7 +50,7 @@ exports.post_new_adoption_application = function(req, res) {
     db.query(q, params, (err, results) => {
        if(err) throw err;
       //console.log("1 record inserted"); 
-       res.send('done :)');
+       res.send('Success!');
     });
 
 };
@@ -132,10 +73,9 @@ exports.post_new_adopter = function(req, res) {
     db.query(q, params, (err, results) => {
        if(err) throw err;
       //console.log("1 record inserted"); 
-       res.send('done');
+       res.send('Success');
     });
 };
-
 
 
 exports.get_adopter = function(req, res) {
@@ -143,7 +83,7 @@ exports.get_adopter = function(req, res) {
     var params = [];
     var q = "SELECT Email_Address FROM Adopter WHERE Email_Address = ? "//+
     
-    params.push(req.params.EmailAddress);
+    params.push(req.query.EmailAddress);
 
     db.query(q, params, (err, result) => {
         var adopter=[];
@@ -160,27 +100,91 @@ exports.get_adopter = function(req, res) {
 };
 
 
-
-exports.get_volunteer_hours = function(req, res) {
+exports.get_applications_number = function(req, res) {
 
     var params = [];
-    var q = "SELECT Username, Date, Hours FROM VolunteerHours WHERE (1=1) AND Username = ? "
-  
-    params.push(req.params.username);
+    var q = `SELECT Application_Number
+    FROM AdoptionApplication 
+    WHERE Email_Address= ? AND Date_Of_Application = ? AND
+    CoApplicant_First_Name = ? AND CoApplicant_Last_Name = ?
+    ORDER BY Application_Number DESC`
+    //////
+    params.push(req.query.EmailAddress);
+    params.push(req.query.DateOfApplication);
+    params.push(req.query.CoApplicantFirstName);
+    params.push(req.query.CoApplicantLastName);
 
-    db.query(q, params, (err, result) => {
-      var volunteerhours=[];
-  
-      if (result!=null) {
-  
-        result.forEach(u => {
-          volunteerhours.push({
-            username: u.Username,
-            date: u.Date,
-            hours: u.Hours,
-           });
-        });
+    db.query(q, params, (err, results) => {
+        var applicationNumber=[];
+
+        if(results!=null) {
+            results.forEach(a => {
+                applicationNumber.push({
+                    applicationNumber: a.Application_Number
+                });
+            });
         }
-      return res.json(volunteerhours);
+        return res.json(applicationNumber[0]);
     });
-  };
+};
+
+
+exports.get_applications_pending_approval = function(req, res) {
+    var params = [];
+    var q = `SELECT Application_Number, Applicant_Fist_Name, Applicant_Last_Name, Street, City, Adopter.State, ZIPCode, Phone_Number, Adopter.Email_Address, CoApplicant_First_Name, CoApplicant_Last_Name 
+    FROM Adopter INNER JOIN AdoptionApplication ON Adopter.Email_Address = AdoptionApplication.Email_Address
+    WHERE AdoptionApplication.State= 'Pending Approval'`
+    
+    db.query(q, params, (err, results) => {
+        var pendingApplications=[];
+
+        if(results!=null) {
+            results.forEach(p => {
+                pendingApplications.push({
+                    applicationNumber: p.Application_Number,
+                    applicantFirstName: p.Applicant_Fist_Name,
+                    applicantLastName: p.Applicant_Last_Name,
+                    street: p.Street,
+                    city: p.City,
+                    state: p.State,
+                    zipCode: p.ZIPCode,
+                    phoneNumber: p.Phone_Number,
+                    emailAddress: p.Email_Address,
+                    coApplicantFirstName: p.CoApplicant_First_Name,
+                    coApplicantLastName: p.CoApplicant_Last_Name
+                });
+            });
+        }
+        return res.json(pendingApplications);
+    });
+};
+
+exports.put_application_status_approve = function(req, res) {
+    var params = [];
+    var q = `UPDATE AdoptionApplication
+    SET   State= 'Approved'
+    WHERE Application_Number = ?`
+    
+    params.push(req.params.applicationNumber);
+
+    db.query(q, params, (err, results) => {
+       if(err) throw err;
+      //console.log("1 record inserted"); 
+       res.send('Approved');
+    });
+};
+
+exports.put_application_status_reject = function(req, res) {
+    var params = [];
+    var q = `UPDATE AdoptionApplication
+    SET   State= 'Rejected'
+    WHERE Application_Number = ?`
+    
+    params.push(req.params.applicationNumber);
+
+    db.query(q, params, (err, results) => {
+       if(err) throw err;
+      //console.log("1 record inserted"); 
+       res.send('Rejected');
+    });
+};
