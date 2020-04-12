e5a03fb7149595165316bb244a9064854a5dc185
Brian Ritchie <britchie3@gatech.edu>
2020-04-12 19:27:38 -0400

    Matched requirements for adoptability checking

--- P3_Teams_Drafts/api/controllers/speciesController.js
+++ P3_Teams_Drafts/api/controllers/speciesController.js
@@ -4,10 +4,23 @@ exports.list_species = function(req, res) {
     var q = `SELECT
                 s.Name,
                 s.Max_Per_Shelter,
-                SUM(IF(a.Adoption_Application_Number is null AND Alteration_Status=1,1,0)) as CountWaitingAdoption,
-                SUM(IF(a.Adoption_Application_Number is null,0,1)) as CountAdopted
+                SUM(Adoptable) as CountWaitingAdoption,
+                COUNT(*)-SUM(Adoptable)-SUM(Adopted) as CountNotReadyForAdoption,
+                SUM(Adopted) as CountAdopted
               FROM Species s
-              LEFT JOIN Animal a ON a.Species=s.Name 
+              LEFT JOIN 
+                (select a.Pet_id, a.Species,
+                ((count(v.vaccine_type) = sum(case when va.pet_id is null then 0 else 1 end)) and 
+                a.alteration_status=1 and
+                a.adoption_application_number is null) as Adoptable,
+                  case when adoption_application_number is null then 0 else 1 end as Adopted
+              from animal a
+              inner join vaccine v on 
+                v.species_name=a.species and require_for_adoption=1
+              left join vaccineadministration va on 
+                va.vaccine_type=v.vaccine_type and a.pet_id=va.pet_id
+              group by a.Pet_ID, a.species) a 
+              ON a.Species=s.Name 
               GROUP BY s.Name, s.Max_Per_Shelter
               ORDER BY s.Name`
   
@@ -21,7 +34,8 @@ exports.list_species = function(req, res) {
             name: row.Name,
             maxPerShelter: row.Max_Per_Shelter,
             countWaitingAdoption: row.CountWaitingAdoption,
-            countAdopted: row.CountAdopted
+            countAdopted: row.CountAdopted,
+            countNotReadyForAdoption: row.CountNotReadyForAdoption
           });
         });
   
